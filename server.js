a012228a983e4195dd21bc7359508468d12d77df
steven rosa <scrosa13@outlook.com>
2022-10-01 18:11:18 -0400

    updating handlebars

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require("path");
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,8 +23,16 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+
 
 app.use(routes);
 
